93d7e98ceef830c3f8f56a2ae79693c3580ef509
MTrunning <1262327911@qq.com>
2020-03-16 21:17:10 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dataV from '@jiaminghi/data-view'
+Vue.use(dataV)
 
 Vue.config.productionTip = false
 
---
+++ src/views/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>主页</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
